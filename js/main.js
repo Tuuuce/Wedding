a567df40ec4c2e8406cbe4f98cfb495082bb8749
Tuuuce <91470624+Tuuuce@users.noreply.github.com>
2021-11-15 23:06:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const navShow = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.menu');
+    const links = document.querySelectorAll('.menu li');
+  
+    burger.addEventListener('click',()=>{
+      nav.classList.toggle('nav-active');
+  
+      links.forEach((link,index)=>{
+        if (link.style.animation) {
+          link.style.animation = '';
+        }else{
+          link.style.animation = `linksFade 0.5s ease forwards ${index / 5 + 0.2}s`;
+        }
+      });
+  
+      burger.classList.toggle('close');
+    });
+  }
+  
+  navShow();
+
+  function updateTimer() {
+    future  = Date.parse("July 09, 2022 11:30:00");
+    now     = new Date();
+    diff    = future - now;
+  
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+  
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+  
+    document.getElementById("timer")
+      .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>minutes</span></div>' +
+        '<div>' + s + '<span>seconds</span></div>' ;
+  }
+  setInterval('updateTimer()', 1000 );
+  
